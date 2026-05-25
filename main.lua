2888264ead478bf0ecd21a8aa709d9912ad2bb25
Majcin9 <marcin.ptasinski@proton.me>
2026-05-25 11:28:26 +0200

    added missing and for if

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@ x, y = 400, 300
 velocity = 0
 transitionSpeed = 5
 theta = 0
-dtheta = 10/(2 * 3.14)
+dtheta = 10 / (2 * 3.14)
 
 function love.load()
 	love.window.setFullscreen(true, "desktop")
@@ -11,27 +11,27 @@ end
 
 function love.update(dt)
 	if love.keyboard.isDown("left") then
-        theta = theta - dtheta*dt
+		theta = theta - dtheta * dt
 	end
 	if love.keyboard.isDown("right") then
-        theta = theta + dtheta*dt
+		theta = theta + dtheta * dt
 	end
-    
 	if love.keyboard.isDown("up") then
-        velocity = velocity + transitionSpeed*dt
+		velocity = velocity + transitionSpeed * dt
 		-- y = y - 100 * dt
-    else if love.keyboard.isDown("down") then
-        velocity = velocity - transisionSpeed*dt
-		y = y + 100 * dt
-    else
-        velocity = velocity - transitionSpeed*dt
-        if velocity < 0 then
-            velocity = 0
-        end
-    end
-
-    x = x + velocity * math.cos(theta)
-    y = y + velocity * math.sin(theta)
+	else
+		if love.keyboard.isDown("down") then
+			velocity = velocity - transisionSpeed * dt
+			y = y + 100 * dt
+		else
+			velocity = velocity - transitionSpeed * dt
+			if velocity < 0 then
+				velocity = 0
+			end
+		end
+	end
+	x = x + velocity * math.cos(theta)
+	y = y + velocity * math.sin(theta)
 end
 
 function love.draw()
