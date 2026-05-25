fc238f9dcf2e5fbfb83751076d5bab5cd0b3e2c6
Mikodomi <mikodominski@gmail.com>
2026-05-25 11:19:58 +0200

    added acceleration and rotation (needs improvement)

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 x, y = 400, 300
+velocity = 0
+transitionSpeed = 5
+theta = 0
+dtheta = 10/(2 * 3.14)
 
 function love.load()
 	love.window.setFullscreen(true, "desktop")
@@ -7,19 +11,29 @@ end
 
 function love.update(dt)
 	if love.keyboard.isDown("left") then
-		x = x - 100 * dt
+        theta = theta - dtheta*dt
 	end
 	if love.keyboard.isDown("right") then
-		x = x + 100 * dt
+        theta = theta + dtheta*dt
 	end
+    
 	if love.keyboard.isDown("up") then
-		y = y - 100 * dt
-	end
-	if love.keyboard.isDown("down") then
+        velocity = velocity + transitionSpeed*dt
+		-- y = y - 100 * dt
+    else if love.keyboard.isDown("down") then
+        velocity = velocity - transisionSpeed*dt
 		y = y + 100 * dt
-	end
+    else
+        velocity = velocity - transitionSpeed*dt
+        if velocity < 0 then
+            velocity = 0
+        end
+    end
+
+    x = x + velocity * math.cos(theta)
+    y = y + velocity * math.sin(theta)
 end
 
 function love.draw()
-	love.graphics.draw(image, x, y)
+	love.graphics.draw(image, x, y, theta)
 end
