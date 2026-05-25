8ba68ddba1402938a1372feb839070d3bd6e88b5
Majcin9 <marcin.ptasinski@proton.me>
2026-05-25 12:35:30 +0200

    removed old code

--- main.lua
+++ main.lua
@@ -10,37 +10,12 @@ k = kart.Kart:new(nil, x, y)
 
 function love.load()
 	love.window.setFullscreen(true, "desktop")
-	image = love.graphics.newImage("assets/gokart3.png")
 end
 
 function love.update(dt)
 	k:update(dt)
-	if love.keyboard.isDown("left") then
-		theta = theta - dtheta * dt
-	end
-	if love.keyboard.isDown("right") then
-		theta = theta + dtheta * dt
-	end
-	if love.keyboard.isDown("up") then
-		velocity = velocity + transitionSpeed * dt
-		-- y = y - 100 * dt
-	else
-		if love.keyboard.isDown("down") then
-			velocity = velocity - transitionSpeed * dt
-			-- y = y + 100 * dt
-		else
-			velocity = velocity - transitionSpeed * dt
-			if velocity < 0 then
-				velocity = 0
-			end
-		end
-	end
-	velocity = math.min(velocity, MaxVelocity)
-	x = x + velocity * math.cos(theta)
-	y = y + velocity * math.sin(theta)
 end
 
 function love.draw()
-	--love.graphics.draw(image, x, y, theta)
 	k:draw()
 end
