6e9c3b014cd5d4ebc4046002ad36f7c89e06d1f7
Mikodomi <mikodominski@gmail.com>
2026-05-26 13:26:52 +0200

    made refactor

--- game.lua
+++ game.lua
@@ -3,50 +3,46 @@ box = require("Box")
 socket = require("socket")
 
 Game = {
-    x = 400,
-    y =  300,
-    velocity = 0,
-    transitionSpeed = 5,
-    theta = 0,
-    dtheta = 15 / (2 * 3.14),
-    MaxVelocity = 10,
-    playersCoords = {}
+    playersCoords = {},
+    mainKart = nil,
+    boxes = {},
+    sock = nil,
 }
+
+Game.__index = Game
 
 function Game:new() 
     ggg = {}
     setmetatable(ggg, self)
-
-end
-
-function Game:update()
-    k2 = kart.Kart:new(100, 100)
-	b = box.Box:new(nil)
-
-    s = socket.connect("localhost", 5000)
+    return ggg
 end
 
 function Game:load()
-	-- k:update(dt)
-    k2:update(dt)
-    if love.keyboard.isDown("e") then
-        k2:shoot()
-    end
-    if s ~= nil then
-        s:send(k2:getPosString())
+    self.mainKart = kart.Kart:new(100, 100)
+    table.insert(self.boxes, box.Box:new(nil))
 
-        playerRaw = s:receive("*l")
-        i = 1
-        playersCoords = {}
+    self.sock = socket.connect("localhost", 5000)
+end
+
+function Game:update(dt)
+	-- k:update(dt)
+    self.mainKart:update(dt)
+    if love.keyboard.isDown("e") then
+        self.mainKart:shoot()
+    end
+    if self.sock ~= nil then
+        self.sock:send(self.mainKart:getPosString())
+
+        local playerRaw = self.sock:receive("*l")
+        self.playersCoords = {}
         while (playerRaw ~= nil and playerRaw ~= "") do
             
-            playerInfo = {}
-            j = 1
+            local playerInfo = {}
             for number in string.gmatch(playerRaw, "%d+") do
                 table.insert(playerInfo, number)
             end
-            table.insert(playersCoords, playerInfo)
-            playerRaw = s:receive("*l")
+            table.insert(self.playersCoords, playerInfo)
+            playerRaw = self.sock:receive("*l")
         end
     end
 end
@@ -54,11 +50,15 @@ end
 function Game:draw()
 	-- k:draw()
     -- k2:draw()
-    for id,player in ipairs(playersCoords) do
+    for id,player in ipairs(self.playersCoords) do
         -- x is player[2] y is player[3] (for some reason)
         print(id, player[2], player[3], player[4]/1000)
-        love.graphics.draw(k2.image, player[2], player[3], player[4]/1000)
+        love.graphics.draw(self.mainKart.image, player[2], player[3], player[4]/1000)
     end
-	b:draw()
+	self.boxes[1]:draw()
 end
 
+
+return {
+    Game = Game
+}
--- main.lua
+++ main.lua
@@ -3,48 +3,21 @@ box = require("Box")
 socket = require("socket")
 game = require("game")
 
+g = game.Game:new()
+
 
 function love.load()
 	-- love.window.setFullscreen(true, "desktop")
 	-- k = kart.Kart:new(x, y)
-    k2 = kart.Kart:new(100, 100)
-	b = box.Box:new(nil)
-
-    s = socket.connect("localhost", 5000)
+    -- g = game.Game:new()
+    g:load()
 end
 
 function love.update(dt)
 	-- k:update(dt)
-    k2:update(dt)
-    if love.keyboard.isDown("e") then
-        k2:shoot()
-    end
-    if s ~= nil then
-        s:send(k2:getPosString())
-
-        playerRaw = s:receive("*l")
-        i = 1
-        playersCoords = {}
-        while (playerRaw ~= nil and playerRaw ~= "") do
-            
-            playerInfo = {}
-            j = 1
-            for number in string.gmatch(playerRaw, "%d+") do
-                table.insert(playerInfo, number)
-            end
-            table.insert(playersCoords, playerInfo)
-            playerRaw = s:receive("*l")
-        end
-    end
+    g:update(dt)
 end
 
 function love.draw()
-	-- k:draw()
-    -- k2:draw()
-    for id,player in ipairs(playersCoords) do
-        -- x is player[2] y is player[3] (for some reason)
-        print(id, player[2], player[3], player[4]/1000)
-        love.graphics.draw(k2.image, player[2], player[3], player[4]/1000)
-    end
-	b:draw()
+    g:draw()
 end
