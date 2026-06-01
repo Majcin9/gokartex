a12ccdaa53ab08e1c728ea1b6d32723c475609ee
Majcin9 <marcin.ptasinski@proton.me>
2026-06-01 11:54:55 +0200

    added box collision

--- Box.lua
+++ Box.lua
@@ -1,7 +1,7 @@
 Box = {
 	x = 0,
 	y = 0,
-	imagepath = "assets/box.png",
+	imagepath = "assets/boxmini.png",
 	image = nil,
 }
 Box.__index = Box
@@ -9,17 +9,26 @@ function Box:new(x, y, imagepath)
 	local o = {}
 	setmetatable(o, self)
 	o.__index = self
-	local w, h = love.window.getDesktopDimensions()
+	local w, h = love.graphics.getDimensions()
 	o.x = x or math.random(1, w - 1)
 	o.y = y or math.random(1, h - 1)
-	o.image = love.graphics.newImage(imagepath or "assets/box.png")
+	o.image = love.graphics.newImage(imagepath or "assets/boxmini.png")
 	return o
 end
 
+function Box:radius()
+	return self.image:getWidth() / 2 --because texture is a square
+end
 function Box:update() end
 
 function Box:draw()
-	love.graphics.draw(self.image, self.x, self.y, 0, 0.1, 0.1)
+	local width = self.image:getWidth()
+	local height = self.image:getHeight()
+	local radius = math.sqrt((width * width) + (height * height)) / 2
+	local newx = self.x + radius * math.cos((5 * 3.14 / 4))
+	local newy = self.y + radius * math.sin((5 * 3.14 / 4))
+
+	love.graphics.draw(self.image, newx, newy, self.theta)
 end
 
 return {
--- game.lua
+++ game.lua
@@ -5,99 +5,123 @@ socket = require("socket")
 drawing = require("drawing")
 
 Game = {
-    playersCoords = {},
-    bulletCoords = {},
-    mainKart = nil,
-    boxes = {},
-    sock = nil,
-    id = 0
+	playersCoords = {},
+	bulletCoords = {},
+	mainKart = nil,
+	boxes = {},
+	sock = nil,
+	id = 0,
 }
 
 Game.__index = Game
 
-function Game:new() 
-    ggg = {}
-    setmetatable(ggg, self)
-    return ggg
+function Game:new()
+	ggg = {}
+	setmetatable(ggg, self)
+	return ggg
 end
 
 function Game:load()
-    self.mainKart = kart.Kart:new(100, 100)
-    table.insert(self.boxes, box.Box:new(nil))
+	self.mainKart = kart.Kart:new(100, 100)
+	table.insert(self.boxes, box.Box:new(nil))
+	table.insert(self.boxes, box.Box:new(nil))
+	table.insert(self.boxes, box.Box:new(nil))
 
-    self.sock = socket.connect("localhost", 5000)
+	self.sock = socket.connect("localhost", 5000)
 
-    self.id = tonumber(self.sock:receive("*l"))
+	self.id = tonumber(self.sock:receive("*l"))
 end
 
 function Game:update(dt)
 	-- k:update(dt)
-    self.mainKart:update(dt)
+	self.mainKart:update(dt)
 
-    if self.sock ~= nil then
-        local kartStr = "P " .. self.mainKart:getPosString()
-        local bullstr = ""
-        if self.mainKart.bu ~= nil then
-            bullStr = "B " .. self.mainKart.bu:getPosString()
-        else
-            bullStr = "B -1 -1"
-        end
-        self.sock:send(kartStr .. "," .. bullStr)
+	if self.sock ~= nil then
+		local kartStr = "P " .. self.mainKart:getPosString()
+		local bullstr = ""
+		if self.mainKart.bu ~= nil then
+			bullStr = "B " .. self.mainKart.bu:getPosString()
+		else
+			bullStr = "B -1 -1"
+		end
+		self.sock:send(kartStr .. "," .. bullStr)
 
+		local playerRaw = self.sock:receive("*l")
+		self.playersCoords = {}
+		self.bulletCoords = {}
+		while playerRaw ~= nil and playerRaw ~= "" do
+			local playerInfo = {}
+			for number in string.gmatch(playerRaw, "[^%s]+") do
+				table.insert(playerInfo, tonumber(number))
+			end
+			table.insert(self.playersCoords, playerInfo)
+			local bulletRaw = self.sock:receive("*l")
+			local bulletInfo = {}
+			for number in string.gmatch(bulletRaw, "[^%s]+") do
+				table.insert(bulletInfo, tonumber(number))
+			end
+			print("bulletRaw" .. bulletRaw)
+			table.insert(self.bulletCoords, bulletInfo)
+			playerRaw = self.sock:receive("*l")
+		end
+	end
 
-        local playerRaw = self.sock:receive("*l")
-        self.playersCoords = {}
-        self.bulletCoords = {}
-        while (playerRaw ~= nil and playerRaw ~= "") do
-            local playerInfo = {}
-            for number in string.gmatch(playerRaw, "[^%s]+") do
-                table.insert(playerInfo, tonumber(number))
-            end
-            table.insert(self.playersCoords, playerInfo)
-            local bulletRaw = self.sock:receive("*l")
-            local bulletInfo = {}
-            for number in string.gmatch(bulletRaw, "[^%s]+") do
-                table.insert(bulletInfo, tonumber(number))
-            end
-            print("bulletRaw" .. bulletRaw)
-            table.insert(self.bulletCoords, bulletInfo)
-            playerRaw = self.sock:receive("*l")
-        end
-    end
-
-    local tempbu = Bullet:new(0, 0, 0)
-    for id, bullet in ipairs(self.bulletCoords) do         
-        if bullet[1] ~= self.id then
-            if circleCollision(self.mainKart.x, self.mainKart.y, self.mainKart.image:getWidth()/2, bullet[2], bullet[3], tempbu.image:getWidth()/2) then
-                print("BULLET COLLISION")
-            end
-        end
-    end
+	local tempbu = Bullet:new(0, 0, 0)
+	for id, bullet in ipairs(self.bulletCoords) do
+		if bullet[1] ~= self.id then
+			if
+				circleCollision(
+					self.mainKart.x,
+					self.mainKart.y,
+					self.mainKart.image:getWidth() / 2,
+					bullet[2],
+					bullet[3],
+					tempbu.image:getWidth() / 2
+				)
+			then
+				print("BULLET COLLISION")
+			end
+		end
+	end
+	for id, box in ipairs(self.boxes) do
+		if
+			circleCollision(
+				self.mainKart.x,
+				self.mainKart.y,
+				self.mainKart.image:getWidth() / 2,
+				box.x,
+				box.y,
+				box.image:getWidth() / 2
+			)
+		then
+			print("BOX COLLISION")
+		end
+	end
 end
-
 
 function Game:draw()
 	-- k:draw()
-    -- k2:draw()
-    local width = self.mainKart.image:getWidth()
-    local height = self.mainKart.image:getHeight()
-    drawing.drawRotated(100, 100, width, height, 0, self.mainKart.image)
-    for id,player in ipairs(self.playersCoords) do
-        -- love.graphics.draw(self.mainKart.image, player[2], player[3], player[4]/1000)
-        drawing.drawRotated(player[2], player[3], width, height, player[4]/1000, self.mainKart.image)
-    end
+	-- k2:draw()
+	local width = self.mainKart.image:getWidth()
+	local height = self.mainKart.image:getHeight()
+	drawing.drawRotated(100, 100, width, height, 0, self.mainKart.image)
+	for id, player in ipairs(self.playersCoords) do
+		-- love.graphics.draw(self.mainKart.image, player[2], player[3], player[4]/1000)
+		drawing.drawRotated(player[2], player[3], width, height, player[4] / 1000, self.mainKart.image)
+	end
 
-    local tempbu = Bullet:new(0, 0, 0)
-    local bulletWidth = tempbu.image:getWidth()
+	local tempbu = Bullet:new(0, 0, 0)
+	local bulletWidth = tempbu.image:getWidth()
 
-    for id,bullet in ipairs(self.bulletCoords) do
-        drawing.drawRotated(bullet[2], bullet[3], bulletWidth, bulletWidth, bullet[4]/1000, tempbu.image)
-    end
-    love.graphics.points(self.mainKart.x, self.mainKart.y)
-	self.boxes[1]:draw()
+	for id, bullet in ipairs(self.bulletCoords) do
+		drawing.drawRotated(bullet[2], bullet[3], bulletWidth, bulletWidth, bullet[4] / 1000, tempbu.image)
+	end
+	love.graphics.points(self.mainKart.x, self.mainKart.y)
+	for id, box in ipairs(self.boxes) do
+		box:draw()
+	end
 end
 
-
 return {
-    Game = Game
+	Game = Game,
 }
